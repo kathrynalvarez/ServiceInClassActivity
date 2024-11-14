3f5a9d28defec6bb0678bea987238f0daff05e8a
kathrynalvarez <tui78504@temple.edu>
2024-11-14 14:56:40 -0500

    Timer works in LogCat

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,16 +1,51 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerService : TimerService.TimerBinder
+    var isConnected = false
+
+    val serviceConnection = object : ServiceConnection{
+        override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
+            timerService = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isConnected = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (isConnected) {
+                if (timerService.isRunning == true) {
+                    timerService.pause()
+                    findViewById<Button>(R.id.startButton).text = "Start"
+
+                } else {
+                    timerService.start(100)
+                    findViewById<Button>(R.id.startButton).text = "Pause"
+                }
+            }
 
         }
         
